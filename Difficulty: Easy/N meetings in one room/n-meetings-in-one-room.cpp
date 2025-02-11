e7cae757dd98a18eab5ef0653ff06bba848bc506
acharNagarajp01 <149056520+nagarajp2004@users.noreply.github.com>
2025-02-11 23:45:20 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Easy/N meetings in one room/n-meetings-in-one-room.cpp
@@ -0,0 +1,66 @@
+//{ Driver Code Starts
+#include <bits/stdc++.h>
+using namespace std;
+
+
+// } Driver Code Ends
+class Solution {
+  public:
+    // Function to find the maximum number of meetings that can
+    // be performed in a meeting room.
+    int maxMeetings(vector<int>& start, vector<int>& end) {
+        // Your code here
+        vector<pair<int,int>>p;
+        int n=start.size();
+        for(int i=0;i<n;i++)
+        {
+            p.push_back({end[i],start[i]});
+        }
+        sort(p.begin(),p.end());
+        
+        int cnt=1;
+        int last_time=p[0].first;
+        
+        for(int i=1;i<n;i++){
+            if(p[i].second>last_time){
+                cnt++;
+                last_time=p[i].first;
+            }
+        }
+        return cnt;
+    }
+};
+
+//{ Driver Code Starts.
+
+int main() {
+
+    int t;
+    cin >> t;
+    cin.ignore();
+    while (t--) {
+        string input;
+        int num;
+        vector<int> start;
+
+        getline(cin, input);
+        stringstream s2(input);
+        while (s2 >> num) {
+            start.push_back(num);
+        }
+
+        vector<int> end;
+        getline(cin, input);
+        stringstream s22(input);
+        while (s22 >> num) {
+            end.push_back(num);
+        }
+
+        Solution ob;
+        int ans = ob.maxMeetings(start, end);
+        cout << ans << endl;
+        cout << "~" << endl;
+    }
+    return 0;
+}
+// } Driver Code Ends
