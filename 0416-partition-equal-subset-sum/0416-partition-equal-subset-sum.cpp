bb86c19e3028a10a29fe231436a09c4dc445b0a0
nagarajp <149056520+nagarajp2004@users.noreply.github.com>
2025-04-07 16:13:21 +0530

    Time: 3 ms (99.62%), Space: 14.3 MB (84.33%) - LeetHub

--- 0416-partition-equal-subset-sum/0416-partition-equal-subset-sum.cpp
+++ 0416-partition-equal-subset-sum/0416-partition-equal-subset-sum.cpp
@@ -1,44 +1,21 @@
 class Solution {
 public:
-    bool recur(int ind,int tar,vector<int>&nums,vector<vector<int>>&dp){
-        if(tar==0){
-            return 1;
-        }
-        if(ind<0){
-            return 0;
-        }
-        if(dp[ind][tar]!=-1){
-            return dp[ind][tar];
-        }
-        if(ind==0){
-          return nums[0]==tar;
-        }
-    bool notake=recur(ind-1,tar,nums,dp);
-       bool take=false;
-        if(tar>=nums[ind])
-        {
-          
-          take=recur(ind-1,tar-nums[ind],nums,dp);
-        }
-
-        return dp[ind][tar]=take | notake;
-    }
-    bool canPartition(vector<int>& nums)
-     {
+    bool canPartition(vector<int>& nums) {
         int sum=0;
-        if(nums.size()<=1){
-            return 0;
-        }
-
-        for(int i=0;i<nums.size();i++)
-        {
-          sum+=nums[i];
-        }
-        if(sum%2!=0){
-            return 0;
+        for(int i=0;i<nums.size();i++){
+            sum+=nums[i];
         }
         int tar=sum/2;
-        vector<vector<int>>dp(nums.size(),vector<int>(tar+1,-1));
-       return recur(nums.size()-1,tar,nums,dp);
+        if(sum%2==1){
+            return false;
+        }
+        bitset<10001>bt(1);
+        for(int i:nums){
+            bt|=bt<<i;
+            if(bt[tar]==1){
+                return true;
+            }
+        }
+        return false;
     }
 };
