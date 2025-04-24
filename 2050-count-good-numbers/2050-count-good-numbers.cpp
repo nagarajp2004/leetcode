fe5d7cc9b19fcbfbf0be4dd4ac1c28089639e5d8
nagarajp <149056520+nagarajp2004@users.noreply.github.com>
2025-04-24 22:11:54 +0530

    Time: 0 ms (100%), Space: 7.8 MB (70.76%) - LeetHub

--- 2050-count-good-numbers/2050-count-good-numbers.cpp
+++ 2050-count-good-numbers/2050-count-good-numbers.cpp
@@ -1,23 +1,25 @@
 class Solution {
 public:
-    long long helper(long long base, long long expo, long long mod) {
-        long long ans = 1;
-        while (expo > 0) {
-            if (expo % 2 == 0) {
-                base = (base * base) % mod;
-                expo = expo / 2;
-            } else {
-                ans = (ans * base) % mod;
-                expo -= 1;
-            }
+int mod=1e9+7;
+    long long  f(long long x,long long p)
+    {
+        if(p==0){
+            return 1;
         }
-        return ans;
+          long long ans=1;
+         while(p){
+               if(p%2==0){
+                x=(x*x)%mod;
+                p/=2;
+               }else{
+                 ans=(ans*x)%mod;
+                 p=p-1;
+               }
+         }
+         return ans%mod;
     }
-
     int countGoodNumbers(long long n) {
-        long long even = (n + 1) / 2;
-        long long odd = n / 2;
-        long long mod = 1e9 + 7;
-        return (int)((helper(5, even, mod) * helper(4, odd, mod)) % mod);
-    }
+        long long even=(n+1)/2,odd=n/2;
+        return ((long long) (f(5.0,even)%mod)*(f(4.0,odd)%mod))%mod; 
+    } 
 };
