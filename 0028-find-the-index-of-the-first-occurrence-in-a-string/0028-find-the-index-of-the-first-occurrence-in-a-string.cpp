fc43dc80921160d861460b2f9d8647c619dc3aea
nagarajp <149056520+nagarajp2004@users.noreply.github.com>
2025-05-07 19:25:29 +0530

    Time: 0 ms (100%), Space: 8.9 MB (13.81%) - LeetHub

--- 0028-find-the-index-of-the-first-occurrence-in-a-string/0028-find-the-index-of-the-first-occurrence-in-a-string.cpp
+++ 0028-find-the-index-of-the-first-occurrence-in-a-string/0028-find-the-index-of-the-first-occurrence-in-a-string.cpp
@@ -1,19 +1,45 @@
 class Solution {
 public:
-    int strStr(string h, string n) 
-    {
-        int n0=h.size();
-        int m0=n.size();
-        for(int i=0;i<n0;i++){
-            int j=0;
-            while(i<n0 && j<m0 && h[i+j]==n[j] ){
-                j++;
-            }
-            if(j==m0){
-                return i;
+    vector<int> find_lps(string n) {
+        int m0 = n.size();
+        int len = 0;
+        vector<int> lps(m0, 0);
+
+        for (int i = 1; i < m0;) {
+            if (n[i] == n[len]) {
+                lps[i++] = ++len;
+            } else if (len != 0) {
+                len = lps[len - 1];
+            } else {
+                lps[i++] = 0;
             }
         }
+
+        return lps;
+    }
+
+    int strStr(string h, string n) {
+        if (n.empty()) return 0;
+
+        vector<int> lps = find_lps(n);
+        int i = 0, j = 0;
+        int n0 = h.size(), m0 = n.size();
+
+        while (i < n0) {
+            if (h[i] == n[j]) {
+                i++; j++;
+            }
+
+            if (j == m0) {
+                return i - j;
+            } else if (i < n0 && h[i] != n[j]) {
+                if (j != 0)
+                    j = lps[j - 1];
+                else
+                    i++;
+            }
+        }
+
         return -1;
-        
     }
 };
