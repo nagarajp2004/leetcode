a6ce5aaf2220e999784d6f47b79ec82780a4fe27
nagarajp <149056520+nagarajp2004@users.noreply.github.com>
2025-05-14 16:54:06 +0530

    Time: 10 ms (7.79%), Space: 23.3 MB (54.93%) - LeetHub

--- 0155-min-stack/0155-min-stack.cpp
+++ 0155-min-stack/0155-min-stack.cpp
@@ -1,31 +1,30 @@
 class MinStack {
 public:
 stack<pair<int,int>>st;
-
     MinStack() {
         
     }
     
-    void push(int val) 
-    {
-       if(st.empty()){
-         st.push({val,val});
-       } 
-       else{
-        st.push({val,min(val,st.top().second)});
-       }
+    void push(int val) {
+        if(st.empty()){
+            st.push({val,val});
+        }else{
+            st.push({val,min(st.top().second,val)});
+        }
     }
     
-    void pop() {
-        st.pop();
+    void pop() { 
+         st.pop();
     }
     
     int top() {
-       return st.top().first;     
+        int x=st.top().first;
+        return x;
     }
     
     int getMin() {
-        return st.top().second;
+        int x=st.top().second;
+        return x;
     }
 };
 
