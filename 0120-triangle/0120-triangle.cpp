67a846c2372d46c90b8eb9cc3ac56c4c63a2707c
nagarajp <149056520+nagarajp2004@users.noreply.github.com>
2025-05-22 17:23:05 +0530

    Time: 0 ms (100%), Space: 13.2 MB (50.34%) - LeetHub

--- 0120-triangle/0120-triangle.cpp
+++ 0120-triangle/0120-triangle.cpp
@@ -1,27 +1,21 @@
 class Solution {
 public:
-    int recur(int i,int j,int n,vector<vector<int>>& t,vector<vector<int>>& dp)
-    {
-         if(j>i)
-         {
-            return INT_MAX;
-         }
-      if(i==n-1)
-      {
-        return t[i][j];
-      }
-     if(dp[i][j]!=-1){
-        return dp[i][j];
-     }
+    int f(vector<vector<int>>& tr, vector<vector<int>>&dp,int i,int j,int n){
+        
+        if(j>i || i>=n){
+            return 0;
+        }
+  if(dp[i][j]!=-1){
+    return dp[i][j];
+  }
 
- return dp[i][j]= t[i][j]+ min(recur(i+1,j,n,t,dp),recur(i+1,j+1,n,t,dp));
+     return dp[i][j]=min(f(tr,dp,i+1,j,n),f(tr,dp,i+1,j+1,n))+tr[i][j];
 
     }
-  
-    int minimumTotal(vector<vector<int>>& t) {
-        int n=t.size();
-        int m=n;
-        vector<vector<int>> dp(n,vector<int> (n,-1));
-        return recur(0,0,n,t,dp);
+
+    int minimumTotal(vector<vector<int>>& tr) {
+       int n=tr.size();
+       vector<vector<int>>dp(n+1,vector<int>(n+1,-1));
+        return f(tr,dp,0,0,n);
     }
 };
