0805a4004c2bfdd27968b90bfe7db423d64d0fe8
nagarajp <149056520+nagarajp2004@users.noreply.github.com>
2025-06-12 11:21:38 +0530

    Time: 0 ms (100%), Space: 95.1 MB (61.46%) - LeetHub

--- 0004-median-of-two-sorted-arrays/0004-median-of-two-sorted-arrays.cpp
+++ 0004-median-of-two-sorted-arrays/0004-median-of-two-sorted-arrays.cpp
@@ -1,48 +1,31 @@
 class Solution {
 public:
     double findMedianSortedArrays(vector<int>& nums1, vector<int>& nums2) {
-        int ind1=0,ind2=0;
-        int i=0,j=0;
         int n1=nums1.size();
         int n2=nums2.size();
+        if(n1>n2) return findMedianSortedArrays(nums2,nums1);
+        int low=0,high=n1;
+        int left =(n1+n2+1)/2;
         int n=n1+n2;
-        int cnt=0;
-        int index2=n/2;
-        int index1=index2-1;
-        while(i<n1 && j<n2){
-            if(nums1[i]<nums2[j]){
-                if(cnt==index1)ind1=nums1[i];
-                if(cnt==index2)ind2=nums1[i];
-                cnt++;
-                i++;
+        while(low<=high){
+            int mid1=(low+high)>>1;
+            int mid2=(left)-mid1;
+            int l1=INT_MIN,l2=INT_MIN;
+            int r1=INT_MAX,r2=INT_MAX;
+            if(mid1<n1) r1=nums1[mid1];
+            if(mid2<n2) r2=nums2[mid2];
+            if(mid1-1>=0) l1=nums1[mid1-1];
+            if(mid2-1>=0) l2=nums2[mid2-1];
+            if(l1<=r2 && l2<=r1){
+                 if(n%2==1) return max(l1,l2);
+                 return ((double)(max(l1,l2)+min(r1,r2)))/2.0;
             }
-            else{
-                 if(cnt==index1)ind1=nums2[j];
-                if(cnt==index2)ind2=nums2[j];
-                cnt++;
-                j++;
-            }}
-            while(i<n1){
-               if(cnt==index1)ind1=nums1[i];
-                if(cnt==index2)ind2=nums1[i];
-                cnt++;
-                i++;  
-            }
-            while(j<n2)
-            {
-                 if(cnt==index1)ind1=nums2[j];
-                if(cnt==index2)ind2=nums2[j];
-                cnt++;
-                j++; 
-            }
-            double x=(double)ind1+(double)ind2;
-        if(n%2==0)
-        {
-          return (double)(x/2.0);
+            else if(l1>r2){
+                high=mid1-1;
+            }else
+              low=mid1+1;
+
         }
-        else{
-         return (ind2);
-          }
-       
+        return 0;
     }
 };
