052a34f4a4de9407d9f6a7fc177cb36e77c72bbf
nagarajp <149056520+nagarajp2004@users.noreply.github.com>
2025-06-13 17:16:57 +0530

    Time: 54 ms (21.99%), Space: 75.6 MB (47.73%) - LeetHub

--- 0148-sort-list/0148-sort-list.cpp
+++ 0148-sort-list/0148-sort-list.cpp
@@ -1,72 +1,75 @@
-/**
- * Definition for singly-linked list.
- * struct ListNode {
- *     int val;
- *     ListNode *next;
- *     ListNode() : val(0), next(nullptr) {}
- *     ListNode(int x) : val(x), next(nullptr) {}
- *     ListNode(int x, ListNode *next) : val(x), next(next) {}
- * };
- */
-class Solution {
-public:
-  ListNode* merge_two(ListNode* list1,ListNode* list2)
- {
-        ListNode* ans=new ListNode(0);
-        ListNode* head=ans;
-        while(list1!=NULL && list2!=NULL){
-            if(list1->val <=list2->val){
-                ans->next=list1;
-                ans=list1;
-                list1=list1->next;
-            } 
-            else{
-                ans->next=list2;
-                ans=list2;
-                list2=list2->next;
+    /**
+    * Definition for singly-linked list.
+    * struct ListNode {
+    *     int val;
+    *     ListNode *next;
+    *     ListNode() : val(0), next(nullptr) {}
+    *     ListNode(int x) : val(x), next(nullptr) {}
+    *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+    * };
+    */
+    class Solution {
+    public:
+        ListNode* merge_two(ListNode* left,ListNode* right){
+            ListNode* ans=new ListNode(0);
+            ListNode* head=ans;
+            while(left && right){
+                if(left->val <=right->val){
+                    ans->next=left;
+                    ans=ans->next;
+                    left=left->next;
+                }
+                else{
+                    ans->next=right;
+                    ans=ans->next;
+                    right=right->next;
+                }
             }
+            while(left){
+                ans->next=left;
+                ans=ans->next;
+                left=left->next;
+            }
+
+            while(right){
+                ans->next=right;
+                    ans=ans->next;
+                    right=right->next;
+            }
+    return head->next;
+        }
+        ListNode* merge(ListNode* head){
+            if(head==NULL || head->next==NULL){
+                return head;
+            }
+            
+            ListNode* left=head;
+            ListNode* mid=middle(head);
+            ListNode* right=mid->next;
+            mid->next=NULL;
+            
+            left=merge(left);
+            right=merge(right);
+
+            return merge_two(left,right);
         }
 
-      while(list1){
-        ans->next=list1;
-        ans=list1;
-        list1=list1->next;
-      }
-      while(list2){
-        ans->next=list2;
-        ans=list2;
-        list2=list2->next;
-      }
-   return head->next;
-  }
-   ListNode* middle(ListNode* head){
-     ListNode* fast=head->next,*slow=head;
-     while(fast!=NULL && fast->next!=NULL){
-        fast=fast->next->next;
-        slow=slow->next;
-     }
-     return slow;
-    
-   }
-   ListNode* merge(ListNode* head)
-   {
-    if(head==NULL || head->next==NULL){
-        return  head;
-    }
-    ListNode* mid=middle(head);
-    ListNode* left=head,*right=mid->next;  
-    mid->next=NULL;
-     left=merge(left);
-     right=merge(right);
-     return merge_two(left,right);
-   }
+        ListNode* middle(ListNode* head){
+            if(head==NULL ||head->next==NULL){
+                return head;
+            }
+            ListNode* slow=head;
+            ListNode* fast=head->next->next;
+            while(fast!=NULL && fast->next!=NULL){
+                slow=slow->next;
+                fast=fast->next->next;
+            }
+            return slow;
+        }
 
 
 
-
-
-    ListNode* sortList(ListNode* head) {
-        
-        return merge(head);
-    }
-};
+        ListNode* sortList(ListNode* head) {
+            return merge(head);
+        }
+    };
