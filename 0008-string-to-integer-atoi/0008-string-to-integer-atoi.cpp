33f3a2ff7a6f8f227454256e2b01074f7a4c05c6
nagarajp <149056520+nagarajp2004@users.noreply.github.com>
2025-06-21 10:28:16 +0530

    Time: 2 ms (19.88%), Space: 10.4 MB (5.46%) - LeetHub

--- 0008-string-to-integer-atoi/0008-string-to-integer-atoi.cpp
+++ 0008-string-to-integer-atoi/0008-string-to-integer-atoi.cpp
@@ -1,43 +1,45 @@
 class Solution {
+
 public:
-    void recur(string s,long long& x,int i,int n,int sign){
-        if(i>=n || !isdigit(s[i])){
-            return ;
-        }
-        x=x*10+(s[i]-'0');
-     
-        if((x*sign) <= INT_MIN){
-           x=INT_MIN;
-            return ;
-        }
-        else if((x*sign) >= INT_MAX){
-            x=INT_MAX;
-            return ;
-        }
-       recur(s,x,i+1,n,sign);
+ void recur(string s,int i,long long& x,int sign)
+ {
+    if(s.size()==i || !isdigit(s[i])){
+        return;
     }
-    int myAtoi(string s) 
-    {
-      int n=s.size();
-      int i=0;
-      long long x=0;
-      while(i<n && s[i]==' '){
+   x=x*10+((s[i]-'0'));
+    if((x*sign)<=INT_MIN){
+        x=INT_MIN;
+        return ;
+    }
+    if(x>=INT_MAX){
+        x=INT_MAX;
+        return;
+    }
+    recur(s,i+1,x,sign);
+ }
+
+    int myAtoi(string s) {
+
+   int i=0;
+      while(s[i]==' '){
         i++;
       }  
-    
-      int sign=1;  
-      if(s[i]=='-'){
+  int sign=1;
+    if(s[i]=='-')
+    {
         sign=-1;
         i++;
-      }else if(s[i]=='+'){
-        i++;
-      }
-     
-      recur(s,x,i,n,sign);
-      if(x!=INT_MIN){
-        return x*sign;
-      }
- return x;
+    }else if(s[i]=='+')
+    {
+         i++;
+    }
+    long long x=0;
+    recur(s,i,x,sign);
+    if(sign==-1){
+        return -1*x;
+    }
+    return x;
+
     }
 
 };
