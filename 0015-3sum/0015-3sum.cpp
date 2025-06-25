a000180384fc8595f8830be70075f7f71a5e87e7
nagarajp <149056520+nagarajp2004@users.noreply.github.com>
2025-06-25 21:12:40 +0530

    Time: 1244 ms (9.09%), Space: 291.8 MB (11.96%) - LeetHub

--- 0015-3sum/0015-3sum.cpp
+++ 0015-3sum/0015-3sum.cpp
@@ -1,34 +1,29 @@
 class Solution {
 public:
-    vector<vector<int>> threeSum(vector<int>& a) {
-        sort(a.begin(), a.end());
-        int n = a.size();
-        vector<vector<int>> ans;
-
-        for (int i = 0; i < n - 2; i++) {
-            if (i > 0 && a[i] == a[i - 1]) continue;  // Skip duplicate elements
-
-            int j = i + 1, k = n - 1;
-            while (k > j) {
-                int sum = a[i] + a[j] + a[k];
-                if (sum == 0) {
-                    ans.push_back({a[i], a[j], a[k]});
-                    j++;
-                    k--;
-
-                    // Skip duplicates for j
-                    while (j < k && a[j] == a[j - 1]) j++;
-                    // Skip duplicates for k
-                    while (j < k && a[k] == a[k + 1]) k--;
+    vector<vector<int>> threeSum(vector<int>& nums) {       
+        sort(nums.begin(),nums.end());
+        set<vector<int>>ans;
+         int n=nums.size();
+        for(int i=0;i<n;i++){
+            int j=i+1;
+            int k=n-1;
+            while(j<k){
+                int sum=nums[i]+nums[j]+nums[k];
+                if(sum==0){
+                   ans.insert({nums[i],nums[j],nums[k]});
+                   j++;
+                   k--;
                 }
-                else if (sum > 0) {
-                    k--;
-                }
-                else {
+                else if(sum<0){
                     j++;
                 }
+                else{
+                    k--;
+                }
+
             }
         }
-        return ans;
+        vector<vector<int>>vt(ans.begin(),ans.end());
+        return vt;
     }
 };
