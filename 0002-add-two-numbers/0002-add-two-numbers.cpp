eb52fcba8dfee1637018ae611c75016a1e5896d6
nagarajp <149056520+nagarajp2004@users.noreply.github.com>
2025-07-08 10:51:52 +0530

    Time: 6 ms (9.15%), Space: 77.2 MB (43.13%) - LeetHub

--- 0002-add-two-numbers/0002-add-two-numbers.cpp
+++ 0002-add-two-numbers/0002-add-two-numbers.cpp
@@ -9,58 +9,44 @@
  * };
  */
 class Solution {
- public:
-    ListNode* revers(ListNode* node){
-        if(node==NULL){
-            return NULL;
-        }
-        if(node->next==NULL){
-            return node;
-        }
-        ListNode* prev=NULL,*cur=node,*next;
-        while(cur){
-            next=cur->next;
-            cur->next=prev;
-            prev=cur;
-            cur=next;
-        }
-       return prev;
-    }
+public:
+    ListNode* addTwoNumbers(ListNode* l1, ListNode* l2) {
+      ListNode* dummy=new ListNode(-1);
+      ListNode* cur=dummy;
+      int carry=0;
 
-    ListNode* addTwoNumbers(ListNode*t1, ListNode* t2) {
+   while(l1 && l2){
+       int sum=l1->val+l2->val+carry;
+         carry=sum/10;
+         sum=sum%10;
+        cur->next=new ListNode(sum);
+        cur=cur->next;        
+      l1=l1->next;
+      l2=l2->next;
+   }
+  while(l1){
+     int sum=carry+l1->val;
+        carry=sum/10;
+         sum=sum%10;
+     cur->next=new ListNode(sum);
+     cur=cur->next;
+     l1=l1->next;
+  }
+  while(l2){
+     int sum=carry+l2->val;
+        carry=sum/10;
+         sum=sum%10;
+     cur->next=new ListNode(sum);
+     cur=cur->next;
+     l2=l2->next;
+  }
 
-        ListNode* head=new ListNode(0);
-        ListNode* temp=head;
-        int car=0;
-        while(t1 && t2){
-             int value = car+t1->val +t2->val;
-             car=value/10;
-             value=value%10;
-             head->next=new ListNode(value);
-           head=head->next;
-           t1=t1->next;
-           t2=t2->next;
-        }
-        while(t1){
-             int value = car+t1->val ;
-             car=value/10;
-             value=value%10;
-             head->next=new ListNode(value);
-             head=head->next;
-              t1=t1->next;
-        }
-        while(t2){
-             int value = car+t2->val ;
-             car=value/10;
-             value=value%10;
-             head->next=new ListNode(value);
-             head=head->next;
-              t2=t2->next;
-        }
-        if(car){
-            head->next =new ListNode(car);
-        }
-        return temp->next;
-      
+  if(carry){
+     cur->next=new ListNode(carry);
+  }
+
+
+   return dummy->next;
+
     }
 };
