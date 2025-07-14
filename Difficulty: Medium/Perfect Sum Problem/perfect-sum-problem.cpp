27af5536722248239534d0386f7a3d064803db0d
nagarajp <149056520+nagarajp2004@users.noreply.github.com>
2025-07-14 16:28:38 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Perfect Sum Problem/perfect-sum-problem.cpp
+++ Difficulty: Medium/Perfect Sum Problem/perfect-sum-problem.cpp
@@ -1,70 +1,44 @@
-//{ Driver Code Starts
-#include <bits/stdc++.h>
-using namespace std;
-
-
-// } Driver Code Ends
-
 class Solution {
   public:
-    int f(vector<int>&a,int i,int n,int t, vector<vector<int>>&dp){
-        if(i == n){
-            return t == 0;
-        }
-        if(t<0){
+     int f(int i,vector<int>&arr,int tar)
+     {
+        if(i==0){
+            if(tar==0 && arr[i]==0){
+                return 2;
+            }
+            if(tar==arr[i] || tar==0){
+                return 1;
+            }
+            
             return 0;
         }
-      
-        if(dp[i][t]!=-1){
-            return dp[i][t];
+        
+        if(arr[i]>tar){
+           return f(i-1,arr,tar);    
         }
         
-      int  c1=0;
-        c1=f(a,i+1,n,t,dp);
-        c1+=f(a,i+1,n,t-a[i],dp);
-  
-         return dp[i][t]=c1;
-    }
-   
+        return f(i-1,arr,tar-arr[i]) + f(i-1,arr,tar);
+        
+     }
   
     int perfectSum(vector<int>& arr, int target) {
-        // code here
-        int count=0;
-        int n=arr.size();
-        vector<vector<int>>dp(n,vector<int>(target+1,-1));
-        return f(arr,0,n,target,dp);
+        // code here,
+         int n=arr.size();
+         vector<vector<int>>dp(n+1,vector<int>(target+1,0));
+         dp[0][0]=1;
+         
+         for(int i=1;i<=n;i++){
+             for(int j=0;j<=target;j++){
+               if(j<arr[i-1]){
+                     dp[i][j]=dp[i-1][j];
+                 }
+                 else{
+                     dp[i][j]=dp[i-1][j]+dp[i-1][j-arr[i-1]];
+                 }
+             }
+         }
+         
+         return  dp[n][target];
+        
     }
 };
-
-
-//{ Driver Code Starts.
-
-int main() {
-    int t;
-    cin >> t;
-    cin.ignore(); // Ignore newline character after t
-
-    while (t--) {
-        vector<int> arr;
-        int target;
-        string inputLine;
-
-        getline(cin, inputLine); // Read the array input as a line
-        stringstream ss(inputLine);
-        int value;
-        while (ss >> value) {
-            arr.push_back(value);
-        }
-
-        cin >> target;
-        cin.ignore(); // Ignore newline character after target input
-
-        Solution solution;
-        cout << solution.perfectSum(arr, target);
-        cout << "\n~\n";
-    }
-
-    return 0;
-}
-
-// } Driver Code Ends
