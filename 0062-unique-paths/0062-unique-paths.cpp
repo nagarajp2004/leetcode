0d57ef212da1f33b837e2a04a03816f8b1789213
nagarajp <149056520+nagarajp2004@users.noreply.github.com>
2025-07-20 15:05:28 +0530

    Time: 0 ms (100%), Space: 9.3 MB (57.5%) - LeetHub

--- 0062-unique-paths/0062-unique-paths.cpp
+++ 0062-unique-paths/0062-unique-paths.cpp
@@ -1,19 +1,26 @@
 class Solution {
 public:
-    int recur(int n,int m,vector<vector<int>>&dp){
-        if(n==0 && m==0){
-            return 1;
-        }
-        if(n<0 || m<0){
+   int f(int i,int j,int m,int n,vector<vector<int>>&dp){
+           if(i>m){
+             return 0;
+           }
+           if(j>n){
             return 0;
-        }
-        if(dp[n][m]!=-1){
-            return dp[n][m];
-        }
-        return dp[n][m]=(recur(n-1,m,dp)+recur(n,m-1,dp));
-    }
-    int uniquePaths(int m, int n) {
-        vector<vector<int>> dp(m,vector<int> (n,-1));
-        return recur(m-1,n-1,dp);
+           }
+      
+      if(i==m && j==n){
+        return 1;
+      }
+      
+      if(dp[i][j]!=-1){
+        return dp[i][j];
+      }
+       
+      return dp[i][j]=f(i+1,j,m,n,dp) +f(i,j+1,m,n,dp);    
+           
+   }
+ int uniquePaths(int m, int n) {
+      vector<vector<int>>dp(m+1,vector<int>(n+1,-1));
+        return f(1,1,m,n,dp);
     }
 };
