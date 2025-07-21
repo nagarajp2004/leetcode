e5f1842f3f2416899a90a69f1c1d81314aa21550
nagarajp <149056520+nagarajp2004@users.noreply.github.com>
2025-07-21 11:54:04 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Easy/Minimum number of Coins/minimum-number-of-coins.cpp
+++ Difficulty: Easy/Minimum number of Coins/minimum-number-of-coins.cpp
@@ -1,51 +1,18 @@
-//{ Driver Code Starts
-// Initial Template for C++
-
-#include <bits/stdc++.h>
-using namespace std;
-
-// } Driver Code Ends
 // User function Template for C++
 
-class Solution{
-public:
-    vector<int> minPartition(int N)
-    {
-         vector<int>ans;
+class Solution {
+  public:
+    vector<int> minPartition(int N) {
         // code here
-        vector<int>vt={ 1, 2, 5, 10, 20, 50, 100, 200, 500, 2000 };
-        for(int i=vt.size()-1;i>=0;i--){
-            if(N>=vt[i]){
-               int n=N/vt[i];
-               while(n){
-                   ans.push_back(vt[i]);
-                   n--;
-               }
-                N=N%vt[i];
-                
-            }
-        }
-        return ans;
+        vector<int>den({2000,500,200,100,50,20,10,5,2,1});
+       vector<int>ans;
+       
+       for(int i=0;i<den.size();i++){
+           while(den[i]<=N){
+               ans.push_back(den[i]);
+               N-=den[i];
+           }
+       }
+       return ans;
     }
 };
-
-//{ Driver Code Starts.
-
-int main(){
-    int t;
-    cin>>t;
-    while(t--){
-        int N;
-        cin>>N;
-        
-        Solution ob;
-        vector<int> numbers = ob.minPartition(N);
-        for(auto u: numbers)
-            cout<<u<<" ";
-        cout<<"\n";
-    
-cout << "~" << "\n";
-}
-    return 0;
-}
-// } Driver Code Ends
