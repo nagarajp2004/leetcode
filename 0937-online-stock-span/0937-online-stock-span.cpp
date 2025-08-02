14a9779d8e666317e38868a06b686902f3b8b6e8
nagarajp <149056520+nagarajp2004@users.noreply.github.com>
2025-08-02 18:06:55 +0530

    Time: 43 ms (20.72%), Space: 92 MB (88.68%) - LeetHub

--- 0937-online-stock-span/0937-online-stock-span.cpp
+++ 0937-online-stock-span/0937-online-stock-span.cpp
@@ -1,19 +1,18 @@
 class StockSpanner {
 public:
-  stack<pair<int,int>>st;
+stack<pair<int,int>>st;
     StockSpanner() {
-      
+        
     }
     
-    int next(int price)
-    {
-     int cnt=1;
-     while(!st.empty() && st.top().first <= price){
-       cnt+=st.top().second;
-       st.pop();
-     }
-     st.push({price,cnt});
-    return cnt;
+    int next(int price) {
+        int tot=0;
+        while(!st.empty() && st.top().first <= price){
+            tot+=st.top().second;
+            st.pop();
+        }
+        st.push({price,tot+1});
+        return st.top().second;
     }
 };
 
