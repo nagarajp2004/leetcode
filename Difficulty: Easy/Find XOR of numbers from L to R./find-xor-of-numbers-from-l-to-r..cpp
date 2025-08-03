75f72345f6aed8ab8938e22c7e6c4e6f6829531c
nagarajp <149056520+nagarajp2004@users.noreply.github.com>
2025-08-03 10:19:44 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Easy/Find XOR of numbers from L to R./find-xor-of-numbers-from-l-to-r..cpp
+++ Difficulty: Easy/Find XOR of numbers from L to R./find-xor-of-numbers-from-l-to-r..cpp
@@ -1,52 +1,29 @@
-//{ Driver Code Starts
-// Initial Template for C++
-#include <bits/stdc++.h>
-using namespace std;
-
-
-// } Driver Code Ends
 // User function Template for C++
-
 
 class Solution {
   public:
-  int ansxor(int n){
-      if(n%4==1){
-          return 1;
-      }
-      if(n%4==2){
-          return n+1;
-          
-      }
-      if(n%4==3){
-          return 0;
-      }
-      if(n%4==0){
-          return n;
-      }
-  }
+     int f(int n){
+         if(n%4==1){
+             return 1;
+         }
+         if(n%4 ==2){
+             return n+1;
+             
+         }
+         
+         if(n%4 ==3){
+             return 0;
+         }
+         
+         if(n%4==0){
+             return n;
+         }
+     }
+   
+  
     int findXOR(int l, int r) {
         // complete the function here
-        return ansxor(r)^ansxor(l-1);
+       return f(r)^f(l-1);
         
     }
 };
-
-//{ Driver Code Starts.
-int main() {
-    int t = 1;
-    cin >> t;
-
-    while (t--) {
-        // Input
-
-        int l, r;
-        cin >> l >> r;
-
-        Solution obj;
-        cout << obj.findXOR(l, r) << endl;
-    
-cout << "~" << "\n";
-}
-}
-// } Driver Code Ends
