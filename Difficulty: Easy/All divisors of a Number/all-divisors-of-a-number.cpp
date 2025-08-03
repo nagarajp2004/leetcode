dee6d32903f49536e323d23c210aeea9cd65cf10
nagarajp <149056520+nagarajp2004@users.noreply.github.com>
2025-08-03 16:08:22 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Easy/All divisors of a Number/all-divisors-of-a-number.cpp
+++ Difficulty: Easy/All divisors of a Number/all-divisors-of-a-number.cpp
@@ -1,45 +1,24 @@
-//{ Driver Code Starts
-// Initial Template for C++
-#include <bits/stdc++.h>
-using namespace std;
-
-// } Driver Code Ends
 class Solution {
-  public:  
+  public:
     void print_divisors(int n) {
         // Code here.
-       vector<int> ans; // To store larger divisors
-        int limit = sqrt(n);
+    
+        vector<int>ans;
         
-        for (int i = 1; i <= limit; i++) {
-            if (n % i == 0) {
-                cout << i << " "; // Print smaller divisor
-                if (i != n / i) { // Avoid duplicate for perfect squares
-                    ans.push_back(n / i);
+        int sq=sqrt(n);
+        
+        for(int i=1;i<=sq;i++){
+            if(n%i==0){
+                cout<<i<<" ";
+                if(i!=(n/i)){
+                    ans.push_back(n/i);
                 }
             }
         }
+        reverse(ans.begin(),ans.end());
         
-        // Print the larger divisors in reverse order
-        for (int i = ans.size() - 1; i >= 0; i--) {
-            cout << ans[i] << " ";
+        for(int i=0;i<ans.size();i++){
+            cout<<ans[i]<<" ";
         }
     }
 };
-
-//{ Driver Code Starts.
-int main() {
-    int T;
-    cin >> T;
-    while (T--) {
-        int n;
-        cin >> n;
-        Solution ob;
-        ob.print_divisors(n);
-        cout << endl;
-    
-cout << "~" << "\n";
-}
-    return 0;
-}
-// } Driver Code Ends
