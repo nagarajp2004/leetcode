0de39833d31b69454822d8f38ecbb3272c83d476
nagarajp <149056520+nagarajp2004@users.noreply.github.com>
2025-08-03 23:01:04 +0530

    Time: 0 ms (100%), Space: 17.9 MB (54.58%) - LeetHub

--- 0081-search-in-rotated-sorted-array-ii/0081-search-in-rotated-sorted-array-ii.cpp
+++ 0081-search-in-rotated-sorted-array-ii/0081-search-in-rotated-sorted-array-ii.cpp
@@ -1,42 +1,39 @@
 class Solution {
 public:
-    bool search(vector<int>& nums, int target) {
-       int i=0;
-       int j=nums.size()-1;
-       
-      
-       while(j>=i){
-          int mid=i+(j-i)/2;
-        
+    bool search(vector<int>& nums, int tar) {
+        int i = 0;
+        int j = nums.size() - 1;
 
-         
-        if(nums[mid]==target){
-            return true;
+        while(i <= j) {
+            int mid = (i + j) / 2;
+
+            if(nums[mid] == tar) return true;
+
+            // Handle duplicates
+            if(nums[i] == nums[mid] && nums[mid] == nums[j]) {
+                i++;
+                j--;
+                continue;
+            }
+
+            // Left half is sorted
+            if(nums[i] <= nums[mid]) {
+                if(nums[i] <= tar && tar < nums[mid]) {
+                    j = mid - 1;
+                } else {
+                    i = mid + 1;
+                }
+            }
+            // Right half is sorted
+            else {
+                if(nums[mid] < tar && tar <= nums[j]) {
+                    i = mid + 1;
+                } else {
+                    j = mid - 1;
+                }
+            }
         }
 
-        else  if(nums[i]==nums[mid] && nums[mid]==nums[j])
-         {
-            i=i+1;
-            j=j-1;
-            continue;
-         }
-        else if(nums[i]>nums[mid]){
-               if(nums[mid]<=target && nums[j]>=target){
-                i=mid+1;
-               }
-               else{
-                j=mid-1;
-               }
-        }
-        else{
-                  if(nums[i]<=target && nums[mid]>=target){
-                    j=mid-1;
-                  }
-                  else{
-                    i=mid+1;
-                  }
-        }
-       }
-       return false; 
+        return false;
     }
 };
