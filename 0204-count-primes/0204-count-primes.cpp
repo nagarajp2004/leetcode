46e683126bb0e878e2cbe35c2a97f0b7ca4767d3
nagarajp <149056520+nagarajp2004@users.noreply.github.com>
2025-08-03 17:02:01 +0530

    Time: 187 ms (53.66%), Space: 256.8 MB (10.52%) - LeetHub

--- 0204-count-primes/0204-count-primes.cpp
+++ 0204-count-primes/0204-count-primes.cpp
@@ -1,26 +1,25 @@
 class Solution {
 public:
- int se(int n){
-   vector<long long>ans(n+1,1);
-   for(long i=2;i<n;i++)
-   {
-      if(ans[i]==1){
-       for(long long j=i*i;j<n;j+=i)
-       {
-         ans[j]=0;
-       }
-       }
-   }
-   int count=0;
-   for(int i=2;i<n;i++){
-    if(ans[i]==1){
-        count++;
-    }
-   }
-   return count;
- }
     int countPrimes(int n) {
-        return se(n);
+        
+    vector<long long>vt(n+1,1);
+     int sq=sqrt(n);
+
+     for(long long i=2;i<=sq;i++){
+
+          if(vt[i]==1){
+            for(long long j=i*i;j<=n;j=j+i){
+                vt[j]=0;
+            }
+          }
+     }
+
+     int count=0;
+     for(int i=2;i<n;i++){
+         count+=vt[i];
+     }
+
+    return count;
 
     }
 };
