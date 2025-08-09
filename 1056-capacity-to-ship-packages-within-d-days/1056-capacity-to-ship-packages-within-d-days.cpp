76a279aef219b8b979339f93f223648dbf9514a2
nagarajp <149056520+nagarajp2004@users.noreply.github.com>
2025-08-09 16:55:59 +0530

    Time: 19 ms (12.41%), Space: 35.2 MB (10.48%) - LeetHub

--- 1056-capacity-to-ship-packages-within-d-days/1056-capacity-to-ship-packages-within-d-days.cpp
+++ 1056-capacity-to-ship-packages-within-d-days/1056-capacity-to-ship-packages-within-d-days.cpp
@@ -1,36 +1,47 @@
 class Solution {
 public:
+    int check(vector<int>& arr,int w){
+          int days=0;
+         
+         int x=0;
+        for(int i=0;i<arr.size();i++)
+        { 
+            if(arr[i] > w){
+                return INT_MAX;
+            }
 
-    int f(vector<int>& w,int maxi){
-       int x=1;
-       int sum=0;
-       for(int i=0;i<w.size();i++){
-         if(sum+w[i]>maxi){
-          x++;
-          sum=w[i];
-         }
-         else{
-            sum+=w[i];
-         }
-       }
-       
-       return x;
-    }
-    int shipWithinDays(vector<int>& w, int days) {
-        int low=*max_element(w.begin(),w.end());
-        int ans=0;
-        int high=accumulate(w.begin(),w.end(),0);
-        while(high>=low){
-            int mid=(high+low)/2;
-            int x=f(w,mid);
-            if(x<=days){
-              high=mid-1;
-            }
-            else{
-              
-              low=mid+1;
-            }
+            x+=arr[i];
+             if(x>w){
+                days++;
+                x=arr[i];
+             }
         }
-        return low ;
+        if(x>0){
+            days++;
+        }
+        return days;
+          
+    }
+
+    int shipWithinDays(vector<int>& w, int days) {
+        
+        int total=accumulate(w.begin(),w.end(),0);
+        int i=1;
+        int j=total;
+         int ans=total;
+
+        while(i<=j){
+            int mid=(i+j)/2;
+            int d=check(w,mid);
+
+            if(d<=days){
+                ans=mid;
+                j=mid-1;
+            }else{
+                i=mid+1;
+            }
+            
+        }
+        return ans;
     }
 };
