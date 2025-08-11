ac82022fc3ddb78eb209eb2d61672d539cd33aef
nagarajp <149056520+nagarajp2004@users.noreply.github.com>
2025-08-11 20:42:06 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Aggressive Cows/aggressive-cows.cpp
+++ Difficulty: Medium/Aggressive Cows/aggressive-cows.cpp
@@ -1,83 +1,45 @@
-//{ Driver Code Starts
-// Initial function template for C++
-
-#include <bits/stdc++.h>
-using namespace std;
-
-
-// } Driver Code Ends
-// User function Template for C++
-
 class Solution {
   public:
-    bool canweplace(vector<int>&s,int dis,int k){
-        int cntcow=1;
-        int last=s[0];
-        for(int i=1;i<s.size();i++){
-            if(s[i]-last>=dis){
-                cntcow++;
-                last=s[i];
-            }
-        }
-        if(cntcow>=k){
-            return true;
-        }
-        return false;
-    }
-
-    int aggressiveCows(vector<int> &s, int k) {
-
-        // Write your code here
-        int maxi=*max_element(s.begin(),s.end());
-        int mini=*min_element(s.begin(),s.end());
-        sort(s.begin(),s.end());
-        int low=1;
-        int high=maxi-mini;
-        while(high>=low){
-            int mid=(high+low)/2;
-            if(canweplace(s,mid,k)){
-                low=mid+1;
-            }
-            else{
-                high=mid-1;
-            }
-        }
-     return high;
-         
-        }
     
-};
-
-//{ Driver Code Starts.
-
-int main() {
-    int test_case;
-    cin >> test_case;
-    cin.ignore();
-    while (test_case--) {
-
-        int k;
-        vector<int> arr;
-        string input;
-        getline(cin, input);
-        stringstream ss(input);
-        int number;
-        while (ss >> number) {
-            arr.push_back(number);
-        }
-        string in;
-        getline(cin, in);
-        stringstream sss(in);
-        int num;
-        while (sss >> num) {
-            k = num;
-        }
-        Solution ob;
-        int ans = ob.aggressiveCows(arr, k);
-        cout << ans << endl;
-        cout << "~" << endl;
+    bool f(int val,int k,vector<int>&st){
+     int count=1;
+     int prev=st[0];
+     int n=st.size();
+     
+     for(int i=0;i<n;i++){
+         if(st[i]-prev >= val){
+             count++;
+             prev=st[i];
+         }
+         if(count >=k){
+             return true;
+         }
+     }
+     return false;
+            
     }
-    return 0;
-}
 
-// } Driver Code Ends
+    int aggressiveCows(vector<int> &stalls, int k) {
+        // code here
+        
+        sort(stalls.begin(),stalls.end());
+        
+        int i=1;
+        int n=stalls.size();
+        int j=stalls[n-1]-stalls[0];
+        int ans=1;
+        while(i<=j){
+            int mid=(i+j)/2;
+            
+            if(f(mid,k,stalls)==true)
+            {
+                ans=mid;
+                i=mid+1;
+            }else{
+                j=mid-1;
+            }
+        }
+        return ans;
+        
+    }
+};
