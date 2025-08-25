8472d30d9912be93c2de63a771698601608b392f
nagarajp <149056520+nagarajp2004@users.noreply.github.com>
2025-08-25 12:10:37 +0530

    Time: 10 ms (76.45%), Space: 17.6 MB (17.88%) - LeetHub

--- 0236-lowest-common-ancestor-of-a-binary-tree/0236-lowest-common-ancestor-of-a-binary-tree.cpp
+++ 0236-lowest-common-ancestor-of-a-binary-tree/0236-lowest-common-ancestor-of-a-binary-tree.cpp
@@ -9,26 +9,31 @@
  */
 class Solution {
 public:
-     TreeNode* dfs(TreeNode* root,TreeNode* p,TreeNode* q){
-        if(root==p || root==q || root==NULL){
-           return root;
-        }   
-     TreeNode* l=dfs(root->left,p,q);
-     TreeNode* r=dfs(root->right,p,q);
-    if(l!=NULL && r!=NULL){
-         return root;
-    }
-     if(l){
-        return l;
-     }
-   if(r){
-     return r;
-   }
- return NULL;
-     }
+    TreeNode* f(TreeNode* root,TreeNode* p,TreeNode* q){
+        if(root==NULL){
+            return NULL;
+        }
+        if(root== p || root ==q) 
+        {
+            return root;
+        }
+        TreeNode* left = f(root->left,p,q);
+         TreeNode* right=f(root->right,p,q);
+        if(left!=NULL && right!=NULL){
+        return root;
+     } 
+        if(left !=NULL ){
+            return left;
+        }
+        if(right!=NULL){
+            return right;
+        }
+        
 
+     return  NULL;
+    }
 
     TreeNode* lowestCommonAncestor(TreeNode* root, TreeNode* p, TreeNode* q) {
-        return dfs(root,p,q);
+        return f(root,p,q);
     }
 };
