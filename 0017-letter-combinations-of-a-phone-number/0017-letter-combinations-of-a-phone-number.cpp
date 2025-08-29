569daa3907f45946bf7119e41df21678c54441ae
nagarajp <149056520+nagarajp2004@users.noreply.github.com>
2025-08-29 14:14:34 +0530

    Time: 0 ms (100%), Space: 9.3 MB (39.42%) - LeetHub

--- 0017-letter-combinations-of-a-phone-number/0017-letter-combinations-of-a-phone-number.cpp
+++ 0017-letter-combinations-of-a-phone-number/0017-letter-combinations-of-a-phone-number.cpp
@@ -1,37 +1,34 @@
 class Solution {
 public:
-    void recur(string& d,unordered_map<int,string>&mpp,vector<string>&st,string& temp,int i){
-        if(i==d.size()){
-           st.push_back(temp);
+    void f(int i,string temp,vector<string>&ans,unordered_map<int,string>&mpp,string d){
+        int n=d.size();
+        if(n==0){
             return ;
         }
-        char x=d[i];
-        int val=x-'0';
-        string y=mpp[val];
-        int n=y.size();
-        for(int z=0;z<n;z++){
-            temp.push_back(y[z]);
-            recur(d,mpp,st,temp,i+1);
-            temp.pop_back();
+        if(i>=n){
+            
+            ans.push_back(temp);
+            return ;
+        }
+       
+        for(int j=0;j<mpp[d[i]-'0'].size();j++){
+                f(i+1,temp+mpp[d[i]-'0'][j],ans,mpp,d);
         }
     }
 
     vector<string> letterCombinations(string digits) {
-    unordered_map<int,string>mpp;
-    mpp[2]="abc";
-    mpp[3]="def";
-    mpp[4]="ghi";
-    mpp[5]="jkl";
-    mpp[6]="mno";
-    mpp[7]="pqrs";
-    mpp[8]="tuv";
-    mpp[9]="wxyz";
-    vector<string>st;
-    string temp;
-    if(digits.size()==0){
-        return {};
-    }
-    recur(digits,mpp,st,temp,0);
+        unordered_map<int,string>mpp;
+        mpp[2]="abc";
+        mpp[3]="def";
+        mpp[4]="ghi";
+        mpp[5]="jkl";
+        mpp[6]="mno";
+        mpp[7]="pqrs";
+        mpp[8]="tuv";
+        mpp[9]="wxyz";
+        vector<string>st;
+        string temp;
+    f(0,temp,st,mpp,digits);
     return st;
     }
 };
