2365577087a153c8b77ee9ff8f4814eb67bc8f9b
nagarajp <149056520+nagarajp2004@users.noreply.github.com>
2025-09-05 21:37:15 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Perfect Sum Problem/perfect-sum-problem.cpp
+++ Difficulty: Medium/Perfect Sum Problem/perfect-sum-problem.cpp
@@ -1,44 +1,32 @@
 class Solution {
   public:
-     int f(int i,vector<int>&arr,int tar)
-     {
-        if(i==0){
-            if(tar==0 && arr[i]==0){
-                return 2;
-            }
-            if(tar==arr[i] || tar==0){
-                return 1;
-            }
-            
-            return 0;
-        }
-        
-        if(arr[i]>tar){
-           return f(i-1,arr,tar);    
-        }
-        
-        return f(i-1,arr,tar-arr[i]) + f(i-1,arr,tar);
-        
-     }
+      
+      
+
   
-    int perfectSum(vector<int>& arr, int target) {
-        // code here,
-         int n=arr.size();
-         vector<vector<int>>dp(n+1,vector<int>(target+1,0));
-         dp[0][0]=1;
-         
-         for(int i=1;i<=n;i++){
-             for(int j=0;j<=target;j++){
-               if(j<arr[i-1]){
-                     dp[i][j]=dp[i-1][j];
-                 }
-                 else{
-                     dp[i][j]=dp[i-1][j]+dp[i-1][j-arr[i-1]];
-                 }
+    int perfectSum(vector<int>& arr, int t) {
+        // code here
+        int n=arr.size();
+        vector<vector<int>>dp(n,vector<int>(t+1,0));
+        
+        if(arr[0]==0){
+            dp[0][0]=2;
+        }else{
+            dp[0][0]=1;
+        }
+        
+       if (arr[0] != 0 && arr[0] <= t) dp[0][arr[0]] = 1;
+
+     
+     for(int i=1;i<n;i++){
+         for(int j=0;j<=t;j++){
+             dp[i][j]+=dp[i-1][j];
+             if(arr[i] <= j){
+                 dp[i][j]+=dp[i-1][j-arr[i]];
              }
          }
-         
-         return  dp[n][target];
+     }   
+     
+     return dp[n-1][t];
         
-    }
-};
+    }};
