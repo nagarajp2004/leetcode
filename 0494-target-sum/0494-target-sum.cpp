4962a5750dd79ba79ca052b9e02e3fcd005f3860
nagarajp <149056520+nagarajp2004@users.noreply.github.com>
2025-09-06 17:14:39 +0530

    Time: 421 ms (23.36%), Space: 11.6 MB (83.09%) - LeetHub

--- 0494-target-sum/0494-target-sum.cpp
+++ 0494-target-sum/0494-target-sum.cpp
@@ -1,36 +1,27 @@
 class Solution {
 public:
-    int f(int ind,int tar,vector<int>&nums,vector<vector<int>>&dp)
-    {
-    
-          if (ind == 0) {
-        if (tar == 0 && nums[0] == 0)
-            return 2; // Two ways to partition: include or exclude the element
-        if (tar == 0 || tar == nums[0])
-            return 1; // One way to partition: include or exclude the element
-        return 0; // No way to partition
-    }
-   
-        if(dp[ind][tar]!=-1)
-         return dp[ind][tar];
-        int nottake=f(ind-1,tar,nums,dp);
-        int take=0;
-        if(tar>=nums[ind]){
-           take=f(ind-1,tar-nums[ind],nums,dp);
-        }
-        return dp[ind][tar]=take+nottake;
-    }
-    int findTargetSumWays(vector<int>& nums, int target) {
-        int sum=accumulate(nums.begin(),nums.end(),0);
-      if (sum < target || (sum - target) % 2 != 0) {
-            return 0;
-        }
-        int ans=(sum-target)/2;
-        if(ans<0){
-          return 0;
+    int f(int i,int sum,vector<int>&arr){
+        if(i==0){
+            if(sum ==0 && arr[0]==0){
+                return 2;
+            }
+
+            if(sum+arr[0] ==0){
+                return 1;
+            }else if(sum - arr[0] ==0){
+                 return 1;
+            }else{
+                return 0;
+            }
         }
 
-        vector<vector<int>>dp(nums.size(),vector<int>(ans+1,-1));
-        return f(nums.size()-1,ans,nums,dp);
+        int mi=f(i-1,sum-arr[i],arr);
+        int mx=f(i-1,sum+arr[i],arr);
+        return mx+mi;
+    }
+
+    int findTargetSumWays(vector<int>& nums, int target) {
+        int n=nums.size();
+        return f(n-1,target,nums);
     }
 };
