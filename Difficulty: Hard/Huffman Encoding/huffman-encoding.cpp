deacd556e3830e97bc47356f4d24bc4f33dbf387
nagarajp <149056520+nagarajp2004@users.noreply.github.com>
2025-09-20 10:41:13 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Hard/Huffman Encoding/huffman-encoding.cpp
+++ Difficulty: Hard/Huffman Encoding/huffman-encoding.cpp
@@ -1,103 +1,65 @@
-//{ Driver Code Starts
-#include<bits/stdc++.h>
-using namespace std;
-
-// } Driver Code Ends
-class Solution
-{
-	public:
-	  class Node{
-	     public:
-	     int freq;
-	     char sys;
-	     Node* right,*left;
-	     
-	     Node(int fre,char s){
-	         freq=fre;
-	         sys=s;
-	         left=NULL,right=NULL;
-	     }
-	  };
-	 class cmp{
-	   
-	   public:
-	   bool operator()(Node* a,Node* b){
-	       return a->freq > b->freq;
-	   }
-	 };
-	   void preorder(Node* root,string& temp,vector<string>& ans)
-	   {
-              if(root==NULL){
-                  return ;
-              }
-              
-              if(root->left==NULL && root->right==NULL){
-                  ans.push_back(temp);
-                  return; 
-              }
-              temp.push_back('0');
-              preorder(root->left,temp,ans);
-              temp.pop_back();
-              temp.push_back('1');
-              preorder(root->right,temp,ans);
-              temp.pop_back(); 	       
-	   }
-	   
-	   
-	
-		vector<string> huffmanCodes(string s,vector<int> f,int N)
-		{
-		    // Code here
-        priority_queue<Node*,vector<Node*>,cmp>pq;
-        for(int i=0;i<N;i++)
-        {
-          pq.push(new Node(f[i],s[i]));    
-        }
-        
-        while(pq.size()>1){
-            Node* first=pq.top();
-            pq.pop();
-            Node* second=pq.top();
-            pq.pop();
-            Node* root=new Node(first->freq+second->freq,'$');
-            root->left=first;
-            root->right=second;
-            pq.push(root);
-        }
-        
-         Node* root=pq.top();
-         pq.pop();
+class Solution {
+  public:
+  
+  class Node{
+      public:
+     char c;
+     int data;
+     Node* left;
+     Node* right;
+     Node(char x,int d){
+         c=x;
+         data=d;
+         left=NULL;
+         right=NULL;
+     }
+  };
+  
+  class comp{
+      public:
+      bool operator()(Node* a,Node* b){
+          return a->data > b->data;
+      }
+  };
+  
+  
+  void preorder(Node* root,vector<string>&vt,string s)
+  {
+      if(!root->left && !root->right){
+          vt.push_back(s);
+          return ;
+      }
+      preorder(root->left,vt,s+'0');
+      preorder(root->right,vt,s+'1');
+      
+      
+  }
+    vector<string> huffmanCodes(string s, vector<int> f, int N) {
+        // Code here
+         priority_queue<Node* ,vector<Node*>,comp>pt;
          
-        vector<string>ans;
-        string temp;
+         for(int i=0;i<N;i++){
+             Node* x=new Node(s[i],f[i]);
+             pt.push(x);
+         }
+         
+         while(pt.size() >1){
+             Node* n1=pt.top();
+             int x=n1->data;
+             pt.pop();
+             Node* n2=pt.top();
+             int y=pt.top()->data;
+             pt.pop();
+             
+             Node* mix=new Node('$',x+y);
+             mix->left=n1;
+             mix->right=n2;
+             pt.push(mix);
+         }
+         vector<string>ans;
+         string temp="";
+         preorder(pt.top(),ans,temp);
+         return ans;
         
-        preorder(root,temp,ans);
-        
-        return ans;
-		}
+    }
 };
-
-//{ Driver Code Starts.
-int main(){
-    int T;
-    cin >> T;
-    while(T--)
-    {
-	    string S;
-	    cin >> S;
-	    int N = S.length();
-	    vector<int> f(N);
-	    for(int i=0;i<N;i++){
-	        cin>>f[i];
-	    }
-	    Solution ob;
-	    vector<string> ans = ob.huffmanCodes(S,f,N);
-	    for(auto i: ans)
-	    	cout << i << " ";
-	    cout << "\n";
-    
-cout << "~" << "\n";
-}
-	return 0;
-}
-// } Driver Code Ends
