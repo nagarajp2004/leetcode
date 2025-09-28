5f613928bab5ea4d106e250ebe2fefecb882ed8b
nagarajp <149056520+nagarajp2004@users.noreply.github.com>
2025-09-28 22:59:54 +0530

    Time: 13 ms (41.88%), Space: 11.5 MB (43.22%) - LeetHub

--- 0076-minimum-window-substring/0076-minimum-window-substring.cpp
+++ 0076-minimum-window-substring/0076-minimum-window-substring.cpp
@@ -1,40 +1,41 @@
 class Solution {
 public:
     string minWindow(string s, string t) {
-       int ans=-1;
-       int as=INT_MAX;
-        set<char>x;
-      unordered_map<char, int> mpp;
+       int n=s.size();
+       int m=t.size();
 
-      for(auto& i:t){
-        mpp[i]++;
-      }
-
-
-    int n=s.size();
-    int i=0,j=0;
-    int total=t.size();
-    while(j<n){
-         mpp[s[j]]--;
-         if(mpp[s[j]]>=0){
-            total--;
+       unordered_map<char,int>mpp;
+       for(auto i:t)
+       {
+        mpp[i]--;
+       }   
+         int spos=-1;
+         int len=INT_MAX;
+       int neg=mpp.size();
+       int i=0;
+       int j=0;
+       while(j<n){
+         mpp[s[j]]++;
+         if(mpp[s[j]] ==0){
+            neg--;
          }
-                 while(!total && i<=j){
-                       if(as>(j-i+1)){
-                        as=j-i+1;
-                        ans=i;
-                       }
-                       mpp[s[i]]++;
-                       if(mpp[s[i]]>0){
-                         total++;
-                       }
-                      i++;
-                 }
-         j++;
-    }
-     if(ans!=-1){
-        return s.substr(ans,as);
-     }
-       return "";
+         while(neg==0){
+             if(len > j-i+1){
+                len=j-i+1;
+                spos=i;
+             }
+          
+                mpp[s[i]]--;
+                if(mpp[s[i]]==-1){
+                    neg++;
+                }
+                i++;
+            } 
+         
+       j++;
+
+       }
+        return (spos == -1) ? "" : s.substr(spos, len);
+         
     }
 };
