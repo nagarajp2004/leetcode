7408e81ce69bcb6f72edb0311dce7a45c906a7ac
nagarajp <149056520+nagarajp2004@users.noreply.github.com>
2025-10-01 09:46:32 +0530

    Time: 0 ms (100%), Space: 11.3 MB (10.93%) - LeetHub

--- 0201-bitwise-and-of-numbers-range/0201-bitwise-and-of-numbers-range.cpp
+++ 0201-bitwise-and-of-numbers-range/0201-bitwise-and-of-numbers-range.cpp
@@ -1,13 +1,13 @@
 class Solution {
 public:
-    int rangeBitwiseAnd(int left, int right)
-    {
-          int shift=0;
-          while(left<right){
-           left=left>>1;
-           right=right>>1;
-           shift++;
-          }
-          return left<<shift;
+    int rangeBitwiseAnd(int left, int right) {
+        
+        int shift=0;
+        while(left < right){
+            left=left>>1;
+            right=right>>1;
+            shift++;
+        }
+        return left<<shift;
     }
 };
