d49e53566c032573ba8a5ff4bb199723e2db9f26
nagarajp <149056520+nagarajp2004@users.noreply.github.com>
2025-10-06 16:25:36 +0530

    Time: 3 ms (69.23%), Space: 10 MB (66.33%) - LeetHub

--- 0567-permutation-in-string/0567-permutation-in-string.cpp
+++ 0567-permutation-in-string/0567-permutation-in-string.cpp
@@ -1,34 +1,26 @@
 class Solution {
 public:
-    bool checkInclusion(string s1, string s2)
-     {
+    bool checkInclusion(string s1, string s2) {
         int n1=s1.size();
         int n2=s2.size();
-        if(n2<n1){
-            return false;
+        vector<int>sm1(26,0);
+        vector<int>sm2(26,0);
+
+        for(auto i: s1){
+            sm1[i-'a']+=1;
         }
-        vector<int>hash1(27,-1);
-        vector<int>hash2(27,-1);
-        for(int i=0;i<n1;i++){
-            hash1[s1[i]-'a']++;
-        }
-        int i=0;
-        int j=n1;
-        for(int i=0;i<j;i++){
-            hash2[s2[i]-'a']++;
-        }
-        while(j<n2)
-        {
-          if(hash1==hash2){
-            return true;
-          }
-          hash2[s2[i]-'a']--;
-          hash2[s2[j]-'a']++;
-          i++;
-          j++;
-        }
-        if(hash1==hash2){
-            return true;
+       
+         
+
+        for(int i=0;i<n2;i++){
+          
+            sm2[s2[i]-'a']++;
+            if(i>=n1){
+                sm2[s2[i-n1]-'a']--;
+            }
+            if(sm1==sm2){
+                return true;
+            }
         }
         return false;
     }
