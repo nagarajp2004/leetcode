bf3282caafe86b00848e0307beaac10a09d33fc0
nagarajp <149056520+nagarajp2004@users.noreply.github.com>
2025-10-12 12:39:52 +0530

    Time: 8 ms (48.92%), Space: 11.2 MB (83.11%) - LeetHub

---
+++ 0142-linked-list-cycle-ii/0142-linked-list-cycle-ii.cpp
@@ -0,0 +1,39 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode(int x) : val(x), next(NULL) {}
+ * };
+ */
+class Solution {
+public:
+    ListNode *detectCycle(ListNode *head) {
+        ListNode* slow=head;
+        ListNode* fast=head;
+
+       bool flag=0;
+
+
+
+        while(fast && fast->next && fast->next->next){
+               slow=slow->next;
+               fast=fast->next->next;
+               if(slow==fast){
+                 flag=1;
+                 break;
+               }
+        }
+          if(flag==0){
+            return NULL;
+          }
+
+          slow=head;
+          while(slow !=fast){
+            slow=slow->next;
+            fast=fast->next;
+          }
+          return slow;
+
+    }
+};
