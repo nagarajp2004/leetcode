b30075c0c8b9e514510a35f5899a063a9c9970a6
nagarajp <149056520+nagarajp2004@users.noreply.github.com>
2025-11-02 09:56:19 +0530

    Time: 0 ms (100%), Space: 18.6 MB (78.88%) - LeetHub

--- 0073-set-matrix-zeroes/0073-set-matrix-zeroes.cpp
+++ 0073-set-matrix-zeroes/0073-set-matrix-zeroes.cpp
@@ -1,54 +1,56 @@
 class Solution {
 public:
     void setZeroes(vector<vector<int>>& mat) {
-        
+         int rextra=1;
+         int cextra=1;
+   int n=mat.size();
+   int m=mat[0].size();
+   int val=mat[0][0];
 
 
-
-
-
-
-
-
-   int n = mat.size();              // number of rows
-    int m = mat[0].size();           // number of columns
-
-    vector<int> rows(n, 0);
-    vector<int> cols(m, 0);
-
-    // First pass: mark the rows and columns to be zeroed
-    for (int i = 0; i < n; ++i) {
-        for (int j = 0; j < m; ++j) {
-            if (mat[i][j] == 0) {
-                rows[i] = 1;
-                cols[j] = 1;
+      for(int i=0;i<n;i++){
+        for(int j=0;j<m;j++){
+            if(mat[i][j]==0){
+                if(i==0){
+                    rextra=0;
+                }else if(j==0){
+                    cextra=0;
+                }
+                mat[i][0]=0;
+                mat[0][j]=0;
             }
         }
-    }
+      }
 
-    // Second pass: zero out marked rows and columns
-    for (int i = 0; i < n; ++i) {
-        for (int j = 0; j < m; ++j) {
-            if (rows[i] == 1 || cols[j] == 1) {
-                mat[i][j] = 0;
+      for(int i=1;i<n;i++){
+        for(int j=1;j<m;j++){
+            if(mat[i][0]==0 || mat[0][j]==0){
+                mat[i][j]=0;
             }
         }
+      }
+      if(val==0){
+        for(int i=0;i<n;i++)
+           mat[i][0]=0;
+
+        for(int j=0;j<m;j++){
+            mat[0][j]=0;
+        }   
+      }
+
+      if(cextra==0){
+        for(int i=0;i<n;i++){
+            mat[i][0]=0;
+        }
+      }
+
+      if(rextra==0){
+        for(int i=0;i<m;i++){
+            mat[0][i]=0;
+        }
+      }
+     
+
+
     }
-}
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-    
 };
