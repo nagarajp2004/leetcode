c12dde83ec58007a7bf8b5e9d4f0e3642c16e31f
nagarajp <149056520+nagarajp2004@users.noreply.github.com>
2025-11-02 12:08:02 +0530

    Time: 0 ms (100%), Space: 15.6 MB (67.33%) - LeetHub

--- 0031-next-permutation/0031-next-permutation.cpp
+++ 0031-next-permutation/0031-next-permutation.cpp
@@ -1,6 +1,25 @@
 class Solution {
 public:
     void nextPermutation(vector<int>& nums) {
-        next_permutation(nums.begin(),nums.end());
+        int x=-1;
+        int n=nums.size();
+        for(int i=n-2;i>=0;i--){
+            if(nums[i]<nums[i+1]){
+                x=i;
+                break;
+            }
+        }
+        if(x==-1){
+            reverse(nums.begin(),nums.end());
+            return ;
+        }
+
+        for(int i=n-1;i>=0;i--){
+            if(nums[i]>nums[x]){
+                swap(nums[i],nums[x]);
+                break;
+            }
+        }
+        reverse(nums.begin()+x+1,nums.end());
     }
 };
