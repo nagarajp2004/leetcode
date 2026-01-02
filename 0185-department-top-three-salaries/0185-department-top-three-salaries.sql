8377c6b8203f59c7592adc3c185654a2ff16cba8
nagarajp <149056520+nagarajp2004@users.noreply.github.com>
2026-01-02 21:07:49 +0530

    Time: 817 ms (87.65%), Space: 0B (100%) - LeetHub

--- 0185-department-top-three-salaries/0185-department-top-three-salaries.sql
+++ 0185-department-top-three-salaries/0185-department-top-three-salaries.sql
@@ -1,14 +1,16 @@
-
-WITH new_table AS (
+WITH n_t AS (
     SELECT 
         d.name AS Department,
         e.name AS Employee,
         e.salary AS Salary,
-        DENSE_RANK() OVER(PARTITION BY d.name ORDER BY e.salary DESC) AS ranking
-    FROM Employee e
-    LEFT JOIN Department d
-    ON e.departmentId = d.id
+        DENSE_RANK() OVER (
+            PARTITION BY d.name 
+            ORDER BY e.salary DESC
+        ) AS r
+    FROM Employee AS e
+    left JOIN Department AS d
+        ON e.departmentId = d.id
 )
 SELECT Department, Employee, Salary
-FROM new_table
-WHERE ranking <= 3;
+FROM n_t
+WHERE r <= 3;
